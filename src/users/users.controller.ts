f5211812e1cd04cb1baa6ed5d29ffb3bda99af9e
arkar20 <arkar20011@gmail.com>
2024-04-03 12:26:09 +0630

    persist in db

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import CreateAuthUserDTO from './dtos/auth.dto';
+import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
+  constructor(private userService: UsersService) {}
+
   @Post('')
   register(@Body() body: CreateAuthUserDTO) {
-    return body;
+    this.userService.create(body.name, body.email, body.password);
   }
 }
