748f613941cdd36a8e5b1d6ee13467c9c885d127
arkar20 <arkar20011@gmail.com>
2024-04-23 00:25:29 +0630

    introducting config

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,17 +7,27 @@ import { ReportsModule } from './reports/reports.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/users.entity';
 import { Report } from './reports/reports.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env.' + process.env.NODE_ENV,
+    }),
     MessagesModule,
     MessagesModule,
     UsersModule,
     ReportsModule,
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      entities: [User, Report],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'sqlite',
+        database: config.get('DB_DATABASE'),
+        synchronize: true,
+        entities: [User, Report],
+      }),
     }),
   ],
   controllers: [AppController],
